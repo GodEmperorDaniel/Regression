93f43fc88b9e0050b4193d2f476ac706303aad9d
Jonathan <jonathan.borjesson@protonmail.com>
2020-02-04 13:52:54 +0100

    Interaction From School 04/02
    Början på interactionsystemet

---
+++ Spel_Projekt_1/Assets/Interactions/InteractionSettings.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public struct InteractionSettings
+{
+	public bool Active;
+	public string text;
+	public ImageSettings image;
+	public AudioClip audioClip;
+}
+
+ [System.Serializable]
+public struct ImageSettings
+{
+	public Image screenImage;
+	public float fadeTimer;
+	public float imageTimer;
+}
---
+++ Spel_Projekt_1/Assets/Interactions/Interactions.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactions : MonoBehaviour
+{
+	[Header("Object Settings")]
+
+	public Sprite objectSprite = null;
+
+	[Header("Collider Settings")]
+
+	[Range(0.001f, 10)]
+	public float colliderRadius;
+
+	private Collider2D objectCollider = null;
+
+	[Header("Interaction Setting")]
+
+	public InteractionSettings onEnter;
+	public InteractionSettings onStay;
+	public InteractionSettings onExit;
+	private AudioSource source;
+
+	private void Awake()
+	{
+		if (objectCollider == null)
+		{
+			objectCollider = gameObject.GetComponent<Collider2D>();
+
+			if (objectCollider == null)
+			{
+				Debug.LogError("Could not find any colliders, it will not work");
+				Debug.Break();
+			}
+			else
+			{
+				(objectCollider as CircleCollider2D).radius = colliderRadius;
+			}
+		}
+		if (objectSprite != null)
+		{
+			gameObject.GetComponentInChildren<SpriteRenderer>().sprite = objectSprite;
+		}
+		else
+		{
+			Debug.LogWarning("No sprite set for " + gameObject.name + ", it will not be visable");
+		}
+		if (source == null)
+		{
+			source = gameObject.GetComponentInChildren<AudioSource>();
+			if (source == null)
+			{
+				Debug.LogWarning("No AudioSource found in children, if you deleted child revert to prefab to fix");
+			}
+		}
+
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (onEnter.Active)
+		{
+			if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+			{
+
+			}
+		}
+	}
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		if (onExit.Active)
+		{
+			if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+			{
+
+			}
+		}
+	}
+
+	private void OnTriggerStay2D(Collider2D other)
+	{
+		if (onStay.Active)
+		{
+			if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+			{
+
+			}
+		}
+	}
+}
