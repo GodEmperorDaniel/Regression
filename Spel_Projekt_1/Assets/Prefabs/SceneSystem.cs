1e9e001c987f0c8ea29281bd34f31d5ad1ecbf2d
mercycrew <jarredjohansson@hotmail.com>
2020-02-05 19:20:53 +0100

    Scensystem
    dörr prefab samt scen manager skripten

---
+++ Spel_Projekt_1/Assets/Prefabs/SceneSystem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSystem : MonoBehaviour
+{
+    public string SceneName;
+    public string doorToSpawn;
+    Vector3 SpawnPos;
+    public GameObject player;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(player);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneName);
+            SpawnPosition();
+            collision.transform.position = SpawnPos;
+        }
+    }
+    private Vector3 SpawnPosition()
+    {
+        SpawnPos = GameObject.FindWithTag(doorToSpawn).transform.position;
+        return SpawnPos;
+    }
+}
