e9ba3ca522081d8638e6115acd23e2393b0a4a68
bevve <bebbe_bevve11@hotmail.com>
2020-02-05 13:19:31 +0100

    input arbete/blend tree ani

--- Spel_Projekt_1/Assets/Scripts/CharacterController2d.cs
+++ Spel_Projekt_1/Assets/Scripts/CharacterController2d.cs
@@ -8,7 +8,11 @@ public class CharacterController2d : MonoBehaviour
     CharacterController Player;
 	public KeyCode forwardKey = KeyCode.W;
 	public KeyCode backwardKey = KeyCode.S;
-
+    public KeyCode leftKey = KeyCode.A;
+    public KeyCode rightKey = KeyCode.D;
+    public KeyCode interactionButton = KeyCode.E;
+    public KeyCode interactionButtonGamepad = KeyCode.Joystick1Button1;
+    public Animator Ani;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,32 +25,53 @@ public class CharacterController2d : MonoBehaviour
     {
         keyboardMovement();
         gamepadMovement();
+        getInteractionKey();
     }
 
     private void keyboardMovement()
     {
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(leftKey))
         {
             transform.position += Vector3.left * MovementSpeed * Time.deltaTime;
+            Ani.SetFloat("HorizontalAni", -1);
+            Ani.SetFloat("VerticalAni", 0);
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(rightKey))
         {
             transform.position += Vector3.right * MovementSpeed * Time.deltaTime;
+            Ani.SetFloat("HorizontalAni", 1);
+            Ani.SetFloat("VerticalAni", 0);
         }
         if (Input.GetKey(forwardKey))
         {
             transform.position += Vector3.up * MovementSpeed * Time.deltaTime;
+            Ani.SetFloat("VerticalAni", 1);
+            Ani.SetFloat("HorizontalAni", 0);
         }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(backwardKey))
         {
             transform.position += Vector3.down * MovementSpeed * Time.deltaTime;
+            Ani.SetFloat("VerticalAni", -1);
+            Ani.SetFloat("HorizontalAni", 0);
         }
     }
 
     private void gamepadMovement()
     {
         float x = Input.GetAxis("Horizontal") * MovementSpeed * Time.deltaTime;
+       // Ani.SetFloat("HorizontalAni", Input.GetAxis("Horizontal"));
         float y = Input.GetAxis("Vertical") * MovementSpeed * Time.deltaTime;
+        //Ani.SetFloat("VerticalAni", Input.GetAxis("Vertical"));
         transform.Translate(x, y, 0);
+    }
+
+    public bool getInteractionKey()
+    {
+        if (Input.GetKey(interactionButton) || Input.GetKey(interactionButtonGamepad))
+            return true;
+        else
+        {
+            return false;
+        }
     }
 }
