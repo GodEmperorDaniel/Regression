fe46590950044e65c960dac3649d2b9e4d9b70f8
bevve <bebbe_bevve11@hotmail.com>
2020-02-12 12:45:04 +0100

    scary flashen

---
+++ Spel_Projekt_1/Assets/Scripts/FlickeringLight.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickeringLight : MonoBehaviour
+{
+    Light testLight;
+    public float minWaitTime;
+    public float maxWaitTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        testLight = GetComponent<Light>();
+        StartCoroutine(GetFlashing());
+    }
+
+    private IEnumerator GetFlashing()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
+            testLight.enabled = !testLight.enabled;
+        }
+    }
+}
