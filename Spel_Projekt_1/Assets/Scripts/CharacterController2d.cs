448b3501d9afeba966667160e2429cb68630fb93
bevve <bebbe_bevve11@hotmail.com>
2020-02-14 09:54:41 +0100

    asd

--- Spel_Projekt_1/Assets/Scripts/CharacterController2d.cs
+++ Spel_Projekt_1/Assets/Scripts/CharacterController2d.cs
@@ -31,7 +31,7 @@ public class CharacterController2d : MonoBehaviour
     }
 
     // Update is called once per frame
-    private void Update()
+    private void LateUpdate()
     {
         keyboardMovement();
         gamepadMovement();
--- Spel_Projekt_1/Assets/Scripts/Item.cs
+++ Spel_Projekt_1/Assets/Scripts/Item.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Item
+public class Item : ScriptableObject
 {
     public int id;
     public string title;
     public string description;
     public Sprite icon;
+    //public Item combinesWith;
+    //public Item producesWhenCombined;
     public Dictionary<string, int> stats = new Dictionary<string, int>();
 
 
-    public Item(int id, string title, string description, 
-        Dictionary<string, int> stats)
+    public Item(int id, string title, string description, Dictionary<string, int> stats)
     {
         this.id = id;
         this.title = title;
