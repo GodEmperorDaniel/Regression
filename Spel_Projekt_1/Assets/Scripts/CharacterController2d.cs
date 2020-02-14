eabde2b77b7f4cc5d36c1f12e50903281d92692a
Jonathan <jonathan.borjesson@protonmail.com>
2020-02-14 11:58:09 +0100

    telfondemo osv

--- Spel_Projekt_1/Assets/Scripts/CharacterController2d.cs
+++ Spel_Projekt_1/Assets/Scripts/CharacterController2d.cs
@@ -27,7 +27,6 @@ public class CharacterController2d : MonoBehaviour
     {
         Player = GetComponent<CharacterController>();
         Ani = GetComponentInChildren<Animator>();
-        
     }
 
     // Update is called once per frame
