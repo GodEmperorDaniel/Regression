60e4b04dc9851a8dd42ee701ac9b169caf323989
Mercycrew <jarredjohansson@hotmail.com>
2020-02-21 16:03:24 +0100

    Interactions protetype
    committar så att jag kan prova byta branch och sedan tillbaks för att se om det hjälper min error

---
+++ Spel_Projekt_1/Assets/Interactions/InteractionTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionTest : MonoBehaviour
+{
+    public Transform player;
+    public float distanceToInteract;
+
+    void Update()
+    {
+        if (player)
+        {
+            Vector2 angle = transform.TransformDirection(Vector2.right);
+            Vector2 toOther = player.position - transform.position;
+
+            if (Vector2.Dot(angle, toOther) <= distanceToInteract)
+            {
+                //press e to interact GUI;
+                Debug.Log("The object has been interacted with");
+            }
+        }
+    }
+}
