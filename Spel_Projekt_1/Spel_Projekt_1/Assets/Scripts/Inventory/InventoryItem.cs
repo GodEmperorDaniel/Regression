93b7fcbf4e2d19365d35f59e83c0ed4c96116f50
JonteHemma <jonathan.bojresson@protonmail.com>
2020-03-22 17:38:46 +0100

    Merge remote-tracking branch 'origin/JarredNew' into Working-Branch

---
+++ Spel_Projekt_1/Spel_Projekt_1/Assets/Scripts/Inventory/InventoryItem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Inventory Item")]
+#pragma warning disable CS0660 // Type defines operator == or operator != but does not override Object.Equals(object o)
+#pragma warning disable CS0661 // Type defines operator == or operator != but does not override Object.GetHashCode()
+public class InventoryItem : ScriptableObject {
+#pragma warning restore CS0661 // Type defines operator == or operator != but does not override Object.GetHashCode()
+#pragma warning restore CS0660 // Type defines operator == or operator != but does not override Object.Equals(object o)
+	public string title;
+	public string description;
+	public Sprite sprite;
+	[HideInInspector]
+	public int itemId;
+
+	public static bool operator ==(InventoryItem item1, InventoryItem item2) {
+		var o1 = (object)item1;
+		var o2 = (object)item2;
+
+		if (o1 == null && o2 == null) {
+			return true;
+		} else if (o1 == null || o2 == null) {
+			return false;
+		} else {
+			throw new InvalidOperationException("Items should not be directly compared. Compare their itemId instead. If you do intend to compare directly, use Equals() instead.");
+		}
+	}
+
+	public static bool operator !=(InventoryItem item1, InventoryItem item2) {
+		var o1 = (object)item1;
+		var o2 = (object)item2;
+
+		if (o1 == null && o2 == null) {
+			return false;
+		} else if (o1 == null || o2 == null) {
+			return true;
+		} else {
+			throw new InvalidOperationException("Items should not be directly compared. Compare their itemId instead. If you do intend to compare directly, use Equals() instead.");
+		}
+	}
+}
---
+++ Spel_Projekt_1/Spel_Projekt_1/Assets/Scripts/PlayerStatic.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController2d))]
+[RequireComponent(typeof(Inventory))]
+[DisallowMultipleComponent]
+public class PlayerStatic : MonoBehaviour {
+	public static GameObject playerInstance;
+	public static CharacterController2d controllerInstance;
+	public static Inventory inventoryInstance;
+	public static int DoorIndex;
+	public static long exitID;
+	private static HashSet<string> freezeStack = new HashSet<string>();
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        if (playerInstance == null)
+        {
+            playerInstance = gameObject;
+			controllerInstance = GetComponent<CharacterController2d>();
+			inventoryInstance = GetComponent<Inventory>();
+        }
+        else
+        {
+           Destroy(gameObject);
+        }
+    }
+
+	public static void FreezePlayer(string key) {
+		freezeStack.Add(key);
+		if (controllerInstance) {
+			controllerInstance.enabled = false;
+		}
+	}
+
+	public static void ResumePlayer(string key) {
+		freezeStack.Remove(key);
+		if (controllerInstance && !IsFrozen()) {
+			controllerInstance.enabled = true;
+		}
+	}
+
+	public static bool IsFrozen() {
+		return freezeStack.Count > 0;
+	}
+}
