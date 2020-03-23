03fd26c6d4c653dace8afc1da8a21b9e8e744360
Mercycrew <jarredjohansson@hotmail.com>
2020-03-23 22:17:54 +0100

    Eye Tree
    The Eye in the tree now follows the player.

---
+++ Spel_Projekt_1/Assets/Scripts/HairTreeEye.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HairTreeEye : MonoBehaviour
+{
+    public GameObject leftObject;
+    public BoxCollider2D right;
+    private BoxCollider2D leftCollider;
+    public Vector2 eyePositionLeft;
+    public Vector2 eyePositionMiddle;
+    public Vector2 eyePositionRight;
+    private Transform eye;
+
+    void Start()
+    {
+        eye = GetComponent<Transform>();
+        leftCollider = leftObject.GetComponent<BoxCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Trigger entered");
+        if (collision == leftCollider)
+        {
+            eye.position = eyePositionLeft;
+            Debug.Log("Trigger LEFT");
+        }
+        if(collision == right)
+        {
+            eye.position = eyePositionRight;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Trigger exited");
+        if (collision == leftCollider)
+        {
+            eye.position = eyePositionMiddle;
+        }
+        if(collision == right)
+        {
+            eye.position = eyePositionMiddle;
+        }
+    }
+}
