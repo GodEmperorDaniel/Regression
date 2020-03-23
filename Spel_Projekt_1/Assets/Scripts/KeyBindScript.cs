3943e8b84368304e3996e467280120b675f9a70d
billy <bebbe_bevve11@hotmail.com>
2020-03-23 18:06:30 +0100

    option -> controller and back. controller till controller setup

---
+++ Spel_Projekt_1/Assets/Scripts/KeyBindScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyBindScript : MonoBehaviour
+{
+    private Dictionary<string, KeyCode> keys = new Dictionary<string, KeyCode>();
+
+    public Text up, left, down, right, interactionButton, inventoryButton;
+    void Start()
+    {
+        keys.Add("Up", KeyCode.W);
+        keys.Add("Down", KeyCode.S);
+        keys.Add("Left", KeyCode.S);
+        keys.Add("Right", KeyCode.R);
+        keys.Add("InteractionButton", KeyCode.E);
+        keys.Add("InventoryButton", KeyCode.Q);
+
+        up.text = keys["Up"].ToString();
+        down.text = keys["Down"].ToString();
+        left.text = keys["Left"].ToString();
+        right.text = keys["Right"].ToString();
+        interactionButton.text = keys["InteractionButton"].ToString();
+        inventoryButton.text = keys["InventoryButton"].ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
